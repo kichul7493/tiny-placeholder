0c634bf2171845b4c81b6f0a14d2122cffd535fa
silveriron <kichul7493@gmail.com>
2024-07-30 22:17:41 +0900

    fix: core export 삭제 (당장 사용할 일이 없어서)

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,2 @@
 export { PlaceholderImage, PlaceholderImageDownload } from './components/index'
-export { PlaceholderImageGenerator } from './core/index'
 export type { PlaceholderOptions, Shape, BorderStyle } from './core/index'
