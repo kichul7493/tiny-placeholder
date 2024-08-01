fdfc78ea6c086e7a505b46a8f1101d40ecd625dc
silveriron <kichul7493@gmail.com>
2024-08-01 22:14:11 +0900

    feat: 기본 옵션 상수

---
+++ src/constants/defaultOptions.ts
@@ -0,0 +1,16 @@
+import { PlaceholderOptions } from '../types'
+
+export const defaultOptions: PlaceholderOptions = {
+  width: 200,
+  height: 200,
+  backgroundColor: '#cccccc',
+  textColor: '#333333',
+  text: 'Placeholder',
+  fontSize: 20,
+  fontFamily: 'Arial, sans-serif',
+  borderWidth: 0,
+  borderColor: '#000000',
+  borderStyle: 'solid',
+  borderRadius: 0,
+  shape: 'rectangle',
+}
