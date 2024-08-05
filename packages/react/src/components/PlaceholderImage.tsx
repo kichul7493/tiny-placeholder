777771d62b3f40bfbdf8b5031b3c985cd25a1965
silveriron <kichul7493@gmail.com>
2024-08-05 21:14:41 +0900

    docs: 리액트 라이브러리 컴포넌트 함수 설명문 추가

--- packages/react/src/components/PlaceholderImage.tsx
+++ packages/react/src/components/PlaceholderImage.tsx
@@ -1,32 +1,32 @@
-import {
-  PlaceholderImageGenerator,
-  PlaceholderOptions,
-} from "@tiny-placeholder/core";
-import { useState, useEffect } from "react";
+import { PlaceholderImageGenerator, PlaceholderOptions } from '@tiny-placeholder/core'
+import { useState, useEffect } from 'react'
 
 interface PlaceholderImageProps {
-  options?: Partial<PlaceholderOptions>;
-  alt?: string;
+  options?: Partial<PlaceholderOptions>
+  alt?: string
 }
 
-export const PlaceholderImage = ({
-  options,
-  alt = "Placeholder",
-}: PlaceholderImageProps) => {
-  const [imageUrl, setImageUrl] = useState<string>("");
+/**
+ * Renders a placeholder image using Tiny Placeholder library.
+ *
+ * @component
+ * @example
+ * ```tsx
+ * <PlaceholderImage options={{ width: 300, height: 200 }} alt="Placeholder" />
+ * ```
+ *
+ * @param options - The options for generating the placeholder image.
+ * @param alt - The alternative text for the image.
+ * @returns The JSX element representing the placeholder image.
+ */
+export const PlaceholderImage = ({ options, alt = 'Placeholder' }: PlaceholderImageProps) => {
+  const [imageUrl, setImageUrl] = useState<string>('')
 
   useEffect(() => {
-    const canvas = new PlaceholderImageGenerator(options);
+    const canvas = new PlaceholderImageGenerator(options)
 
-    setImageUrl(canvas.getDataURL());
-  }, [options]);
+    setImageUrl(canvas.getDataURL())
+  }, [options])
 
-  return (
-    <img
-      width={options?.width}
-      height={options?.height}
-      src={imageUrl}
-      alt={alt}
-    />
-  );
-};
+  return <img width={options?.width} height={options?.height} src={imageUrl} alt={alt} />
+}
--- packages/react/src/components/PlaceholderImageDownloader.tsx
+++ packages/react/src/components/PlaceholderImageDownloader.tsx
@@ -1,29 +1,39 @@
-import {
-  PlaceholderImageGenerator,
-  PlaceholderOptions,
-} from "@tiny-placeholder/core";
-import { useState, useEffect } from "react";
+import { PlaceholderImageGenerator, PlaceholderOptions } from '@tiny-placeholder/core'
+import { useState, useEffect } from 'react'
 
 interface PlaceholderImageDownloadProps {
-  options?: Partial<PlaceholderOptions>;
-  component?: React.ReactNode;
+  options?: Partial<PlaceholderOptions>
+  component?: React.ReactNode
 }
 
-export const PlaceholderImageDownload = ({
-  options,
-  component,
-}: PlaceholderImageDownloadProps) => {
-  const [imageUrl, setImageUrl] = useState<string>("");
+/**
+ * Renders a component that generates and downloads a placeholder image.
+ *
+ * @component
+ * @example
+ * ```tsx
+ * <PlaceholderImageDownload
+ * options={{ width: 300, height: 200 }}
+ * component={<button>Click</button>}
+ * />
+ * ```
+ *
+ * @param options - The options for generating the placeholder image.
+ * @param component - The component to render as the download button. If not provided, a default "Download" text will be used.
+ * @returns {JSX.Element} The rendered component.
+ */
+export const PlaceholderImageDownload = ({ options, component }: PlaceholderImageDownloadProps): JSX.Element => {
+  const [imageUrl, setImageUrl] = useState<string>('')
 
   useEffect(() => {
-    const canvas = new PlaceholderImageGenerator(options);
+    const canvas = new PlaceholderImageGenerator(options)
 
-    setImageUrl(canvas.getDataURL());
-  }, [options]);
+    setImageUrl(canvas.getDataURL())
+  }, [options])
 
   return (
     <a href={imageUrl} download="placeholder.png">
-      {component || "Download"}
+      {component || 'Download'}
     </a>
-  );
-};
+  )
+}
